9b097319d9022a308b8baa954b3b1fb485a710a1
Omar Zaki  Nour Eldin <120220392+Ojotah@users.noreply.github.com>
2026-03-14 14:14:30 +0200

    Fix API route registration for feature tests

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\CommentController;
 use App\Http\Controllers\Api\ProjectController;
 use App\Http\Controllers\Api\TaskController;
+use Illuminate\Support\Facades\Route;
 
 Route::patch('/tasks/{id}/status', [TaskController::class, 'changeStatus']);
 Route::apiResource('projects', ProjectController::class);
